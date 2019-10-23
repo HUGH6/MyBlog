f73ec57d64404cd9c4082ec59cd3bb08e937e9b2
huzihan <hzh1941857127@163.com>
2019-10-23 21:37:35 +0800

    update my blog at 2019-10-23 21:37:35

--- my_cli.js
+++ my_cli.js
@@ -3,12 +3,6 @@ let option = process.argv[2]
 
 var exec = require('child_process').exec
 
-// 1、创建文档
-
-// 2、编写完后，执行hexo clean && hexo g && hexo d
-
-// 3、将git仓库的所以变更同步到远程仓库
-
 
 // 命令行
 const GIT_STATUS = 'git status'
@@ -18,48 +12,11 @@ const GIT_PULL = 'git pull origin master'
 const GIT_PUSH = 'git push -u origin master'
 const DEPLOY = 'hexo clean && hexo g && hexo d'
 
-// 功能1：开始编写博客前，与远程仓库的博客源代码进行同步
-function start_work () {
-    console.log("###############################")
-    console.log("step1: 正在从远程仓库同步代码到本地...")
-    console.log("###############################")
-    exec(GIT_PULL, function(err, stdout, stderr) {
-        local_log(err, stdout, stderr)
-    })
-    console.log("###############################")
-    console.log("同步代码结束！")
-    console.log("###############################")
-}
 
-// 功能2：写完博客后，部署博客
-function deploy_website () {
-    console.log("###############################")
-    console.log("step2: 正在更新博客内容并将其部署到github pages...")
-    console.log("###############################")
-    exec(DEPLOY, function(err, stdout, stderr) {
-        local_log(err, stdout, stderr)
-    })
-    console.log("###############################")
-    console.log("部署结束！")
-    console.log("###############################")
-}
-
-// 功能3：将本地博客源代码的修改同步到远程仓库
-function finish_work () {
-    console.log("###############################")
-    console.log("step3: 正在将本地源码的更改同步到远程仓库...")
-    console.log("###############################")
-    cli = GIT_ADD_ALL + ' && ' + GIT_COMMIT + ' && ' + GIT_PUSH
-    exec(cli, function(err, stdout, stderr) {
-        local_log(err, stdout, stderr)
-    })
-    console.log("###############################")
-    console.log("同步博客源代码更改结束！")
-    console.log("###############################")
-}
 
 
 // 功能开始
+// 功能1：开始编写博客前，与远程仓库的博客源代码进行同步
 if (option === 'start' || option === 's') {
     console.log("###############################")
     console.log("step1: 正在从远程仓库同步代码到本地...")
@@ -69,12 +26,13 @@ if (option === 'start' || option === 's') {
             throw err
         }
         console.log(stdout)
-
+        
         console.log("###############################")
         console.log("同步代码结束！")
         console.log("###############################")
     })
     // start_work()
+    // 功能2：写完博客后，部署博客
 } else if (option === 'deploy' || option === 'd') {
     console.log("###############################")
     console.log("step2: 正在更新博客内容并将其部署到github pages...")
@@ -84,12 +42,13 @@ if (option === 'start' || option === 's') {
             throw err
         }
         console.log(stdout)
-
+        
         console.log("###############################")
         console.log("部署结束！")
         console.log("###############################")
     })
     // deploy_website()
+// 功能3：将本地博客源代码的修改同步到远程仓库
 } else if (option === 'finish' || option === 'f') {
     console.log("###############################")
     console.log("step3: 正在将本地源码的更改同步到远程仓库...")
@@ -106,6 +65,7 @@ if (option === 'start' || option === 's') {
         console.log("###############################")
     })
     // finish_work()
+// 操作失败，退出
 } else {
     console.log("###############################")
     console.log("ERR: 参数错误")
